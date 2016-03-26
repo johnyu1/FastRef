f6f8a026489ed9312158581e3e80fbb19ed93c93
Cassidy Burden <deltaburnt@gmail.com>
2016-03-26 17:27:11 -0500

    Keyword searching implemented

--- FastRef/war/js/fastref.js
+++ FastRef/war/js/fastref.js
@@ -8,10 +8,27 @@ keywords = {
 
 $(document).ready(function(event) {
     $(document).keypress(function(event) {
-        if (!$("#keyword").is(":focus")) {
-            if (event.keyCode == 27) {
-                $("#keyword").focus();
+        if (event.keyCode == 27) {
+            $("#keyword").focus();
+        }
+    });
+    
+    $("#keyword").focus(function(event) {
+        this.select();
+    });
+    
+    $("#keyword").keyup(function(event) {
+        var keyword_res = "";
+        var page_res = "";
+        for (keyword in keywords) {
+            if (keyword.toLowerCase() == $(this).val().toLowerCase()) {
+                keyword_res = keyword;
+                page_res = keywords[keyword].page;
+                break;
             }
         }
+        
+        $("p#keyword_res").text("keyword: " + keyword_res);
+        $("p#page_res").text("page: " + page_res);
     });
 });
