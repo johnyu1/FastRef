f986ec845018678b5b3ddddd6eb6afa5cbdbb6de
Leelabrindavanan Karunakaran <leela_k@utexas.edu>
2016-04-22 18:01:51 -0500

    Added script to login to google accounts and persist cookie for tests
    Signed-off-by: Leelabrindavanan Karunakaran <leela_k@utexas.edu>

---
+++ FastRef/System_Tests/login.py
@@ -0,0 +1,9 @@
+from mail import SessionGoogle
+
+user = 'fastreftest@gmail.com'
+pwd = 'fastref123'
+url_login = "https://accounts.google.com/AccountChooser?continue=https%3A%2F%2Fappengine.google.com%2F_ah%2Fconflogin%3Fcontinue%3Dhttp%3A%2F%2F1-dot-fast-ref.appspot.com%2Fupload.jsp&hl=en&service=ah"
+
+url_auth = "https://accounts.google.com/ServiceLogin?continue=https%3A%2F%2Fappengine.google.com%2F_ah%2Fconflogin%3Fcontinue%3Dhttp%3A%2F%2F1-dot-fast-ref.appspot.com%2Fupload.jsp&service=ah&sacu=1&rip=1#password"
+session = SessionGoogle(url_login, url_auth, user, pwd)
+print session.get("http://1-dot-fast-ref.appspot.com/listfiles.jsp")
---
+++ FastRef/System_Tests/mail.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+class SessionGoogle:
+    def __init__(self, url_login, url_auth, login, pwd):
+        self.ses = requests.session()
+        login_html = self.ses.get(url_login)
+        soup_login = BeautifulSoup(login_html.content).find('form').find_all('input')
+        dico = {}
+        for u in soup_login:
+            if u.has_attr('value'):
+                dico[u['name']] = u['value']
+        # override the inputs with out login and pwd:
+        dico['Email'] = login
+        dico['Passwd'] = pwd
+        self.ses.post(url_auth, data=dico)
+
+    def get(self, URL):
+        return self.ses.get(URL).text
+
+
