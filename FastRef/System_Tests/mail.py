e85a05620758b79e3a70804223d437f5abf804b9
Leelabrindavanan Karunakaran <leela_k@utexas.edu>
2016-04-22 18:32:18 -0500

    Fixed login updated environment.py for testing

--- FastRef/System_Tests/mail.py
+++ FastRef/System_Tests/mail.py
@@ -5,6 +5,7 @@ class SessionGoogle:
         self.ses = requests.session()
         login_html = self.ses.get(url_login)
         soup_login = BeautifulSoup(login_html.content).find('form').find_all('input')
+        links = BeautifulSoup(login_html.content).find_all('a')
         dico = {}
         for u in soup_login:
             if u.has_attr('value'):
@@ -13,7 +14,8 @@ class SessionGoogle:
         dico['Email'] = login
         dico['Passwd'] = pwd
         self.ses.post(url_auth, data=dico)
-
+        for link in links:
+        	print link.get('href')
     def get(self, URL):
         return self.ses.get(URL).text
 
