0cca8d5b9a0bbfdbfc910ed0393d95aff00a1ddf
Leelabrindavanan Karunakaran <leela_k@utexas.edu>
2016-04-22 20:41:34 -0500

    Added test files for access in init for tests
    Signed-off-by: Leelabrindavanan Karunakaran <leela_k@utexas.edu>

--- FastRef/System_Tests/steps/fastrefsteps.py
+++ FastRef/System_Tests/steps/fastrefsteps.py
@@ -1,18 +1,18 @@
 @given('User not logged in')
 def step_impl(context):
-
+	context.login = False
 @given('User logged in')
 def step_impl(context):
-
+	context.login = True
 @given('File is public')
 def step_impl(context):
-
+	context.fname = context.publicfname
 @given('File is private')
 def step_impl(context):
-
+	context.fname = context.privatefname
 @when('User accesses file')
 def step_impl(context):
-
+	
 @when('User uploads file')
 def step_impl(context):
 
@@ -24,4 +24,3 @@ def step_impl(context):
 
 @then('status code is {sc}')
 def step_impl(context, sc):
-	
