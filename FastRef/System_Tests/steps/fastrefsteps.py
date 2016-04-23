7229bf2ab94fcebfefefc57f444dff62522c556d
Leelabrindavanan Karunakaran <leela_k@utexas.edu>
2016-04-22 21:11:10 -0500

    Added test cases implemented access tests
    Signed-off-by: Leelabrindavanan Karunakaran <leela_k@utexas.edu>

--- FastRef/System_Tests/steps/fastrefsteps.py
+++ FastRef/System_Tests/steps/fastrefsteps.py
@@ -1,20 +1,27 @@
+import requests
 @given('User not logged in')
 def step_impl(context):
-	context.login = False
+	context.session = requests.session()
 @given('User logged in')
 def step_impl(context):
-	context.login = True
+	context.session = SessionGoogle(url_login, url_auth, user, pwd)
 @given('File is public')
 def step_impl(context):
-	context.fname = context.publicfname
+	context.public = True
 @given('File is private')
 def step_impl(context):
-	context.fname = context.privatefname
+	context.public = False
 @when('User accesses file')
 def step_impl(context):
-	
+	url = context.url + 'serve?blob-key='
+	if context.public:
+		url += context.publid
+	else:
+		url += context.privid
+	context.response = context.session.get(url)
 @when('User uploads file')
 def step_impl(context):
+
 
 @when('User deletes file')
 def step_impl(context):
@@ -24,3 +31,4 @@ def step_impl(context):
 
 @then('status code is {sc}')
 def step_impl(context, sc):
+	assertEqual(context.response.status_code, sc)
