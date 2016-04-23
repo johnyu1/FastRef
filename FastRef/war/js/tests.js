be7ec7ffd31b91a364de9292540fba3f2b626f90
Cassidy Burden <deltaburnt@gmail.com>
2016-04-23 15:02:22 -0500

    Add insert panel test

--- FastRef/war/js/tests.js
+++ FastRef/war/js/tests.js
@@ -36,14 +36,6 @@ QUnit.test("Page Number Checks", function (assert) {
     loadPDF(callback);
 });
 
-QUnit.test("Keyword Exists Test", function (assert) {
-    kw = Object.keys(keywords)[0];
-    notAKW = "doesNotExist";
-    assert.ok(keywordExists(kw) === true, "Check keyword ("+kw+") exists");
-    assert.ok(keywordExists(kw.toLowerCase()) === true, "Check keyword ("+kw+") lowercase exists");
-    assert.ok(keywordExists(notAKW) === false, "Check keyword ("+notAKW+") doesn't exist");
-});
-
 QUnit.test("Keyword Test", function (assert) {
     var done = assert.async();
     callback = function() {
@@ -57,4 +49,18 @@ QUnit.test("Keyword Test", function (assert) {
         done();
     }
     loadPDF(callback);
+});
+
+QUnit.test("Keyword Exists Test", function (assert) {
+    kw = Object.keys(keywords)[0];
+    notAKW = "doesNotExist";
+    assert.ok(keywordExists(kw) === true, "Check keyword ("+kw+") exists");
+    assert.ok(keywordExists(kw.toLowerCase()) === true, "Check keyword ("+kw+") lowercase exists");
+    assert.ok(keywordExists(notAKW) === false, "Check keyword ("+notAKW+") doesn't exist");
+});
+
+QUnit.test("Panel Add Test", function (assert) {
+    insertKeywordPanel("123abc", 50);
+    assert.ok($("#kw-123abc").length !== 0, "Test keyword panel insertion");
+    assert.ok($("#kw-123abc").children(".badge").text() === "Page 50", "Test keyword panel page");
 });
